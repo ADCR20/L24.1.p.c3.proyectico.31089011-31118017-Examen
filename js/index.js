834f62f5979da00d2b5fb815e6257e64c18fd107
ADCR20 <1001.31118017.ucla@gmail.com>
2024-11-18 22:31:14 -0400

    Cambios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-export default Dt_estudiantes = [ 
-    {nombre: "Luis", cedula: 1111, sexo: "M", nota: 12 }, 
-    {nombre: "Carla", cedula: 2222, sexo: "F", nota: 16.5}, 
-    {nombre: "Mery", cedula: 3333, sexo: "F", nota: 8.5 }
-]
+/*Se desea llevar un control de los estudiantes que 
+asisten a presentar un examen. Se tiene por cada 
+participante: nombre, cédula, sexo y nota. Se requiere 
+de un programa que permita el registro de esta 
+información, conociendo al principio de la ejecución el 
+valor del examen y el mínimo aprobatorio.
+
+
+Nombre   Cédula   Sexo    Nota
+Luis      1111     “M”     12
+Carla     2222     “F”    16.5
+Mery      3333     “F”     8.5
+
+*/
